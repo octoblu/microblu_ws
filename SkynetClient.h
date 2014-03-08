b07773b5f6496fc70cf2de4d05d694bef834ac51
Jacob Rosenthal <jakerosenthal@gmail.com>
2014-03-08 00:06:24 -0700

    cleaned up parsing

--- SkynetClient.h
+++ SkynetClient.h
@@ -84,7 +84,7 @@ class SkynetClient  {
 		
 	private:
 	    ring_buffer *_rx_buffer;	
-		void dump(int x);
+		void dump();
 		char *dataptr;
 		char databuffer[DATA_BUFFER_LEN];
 		char sid[SID_LEN];
